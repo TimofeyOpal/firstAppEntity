a7f29d965870bfc7910656c8b0ce257966067601
timofey <timofey.gusev.opal@gmail.com>
2020-09-04 01:15:05 +0400

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace firstAppEntity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           using(AppContext context = new AppContext())
+            {
+                context.Users.Add(new User()
+               {
+                   Name = "Витя", Age = 22});
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace firstAppEntity
+{
+    class User
+    {
+        [Key] //валидация 
+       public int Id { get; set; }
+       public string Name { get; set; }
+       public int Age { get; set; }
+
+    }
+}
